e050595aadc8eebbc3e6251f3a08f15b01aed8f8
Oladeji Olanipekun <oladeji.olanipekun@sterling.ng>
2025-03-14 00:20:52 +0100

    first commit

--- app/(components)/DeleteBlock.jsx
+++ app/(components)/DeleteBlock.jsx
@@ -8,7 +8,7 @@ const DeleteBlock = ({ id }) => {
   const router = useRouter();
 
   const deleteTicket = async () => {
-    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+    const res = await fetch(`/api/Tickets/${id}`, {
       method: "DELETE",
     });
 
--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -1,7 +1,7 @@
 import TickectForm from "@/app/(components)/TickectForm";
 
 const getTicketById = async (id) => {
-  const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+  const res = await fetch(`/api/Tickets/${id}`, {
     cache: "no-cache",
   });
   if (!res.ok) {
